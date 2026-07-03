11dff358428c208aeb20aee59cf0619806013461
BlackOwl <blackowltr@gmail.com>
2026-07-03 09:52:49 +0300

    Add Macopy cask v1.0.0

---
+++ Casks/macopy.rb
@@ -0,0 +1,15 @@
+cask "macopy" do
+  version "1.0.0"
+  sha256 "615103df0bd5229b68fe4542b913b3630bb4a594aade1b7957c6fb11eaddd60f"
+
+  url "https://github.com/blackowltr/Macopy/releases/download/v#{version}/Macopy-#{version}.dmg"
+  name "Macopy"
+  desc "Clipboard history manager for macOS"
+  homepage "https://github.com/blackowltr/Macopy"
+
+  depends_on macos: ">= :monterey"
+
+  app "Macopy.app"
+
+  uninstall quit: "com.blackowltr.macopy"
+end
